d490f2202621b6b0d09c2ec34c38d01b07395b1c
Ubuntu <ubuntu@ip-172-31-30-224.ec2.internal>
2019-09-15 16:44:46 +0000

    Fancy hello world in Cpp

---
+++ Fancy_hello.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 St. Mother Teresa HS All rights reserved
+//
+// Created by Paul Madut
+// Created on September 2019
+// says hello world but looks cooler
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+}
